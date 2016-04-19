51cc188854ffb9749665783b3a35af39f955af25
gwei <gongwei0214@gmail.com>
2016-04-20 00:24:32 +0800

    Add:list

---
+++ src/list.hpp
@@ -0,0 +1,225 @@
+#ifndef __SIMPLE_LIST__
+#define __SIMPLE_LIST__
+
+#include <stddef.h>
+#include "allocator.hpp"
+#include "data_fill.hpp"
+#include "algorithm/base.hpp"
+#include <iostream>
+
+namespace simple
+{
+/**
+  list node
+*/
+template <typename T>
+struct _list_node
+{
+  _list_node<T> *before;
+  _list_node<T> *after;
+  T data;
+  _list_node():before(NULL),after(NULL){}
+
+};
+/**
+ list iterator
+*/
+template <typename T, typename Ref, typename Ptr>
+struct _list_iterator 
+{
+  _list_node<T> *node_ptr_;
+  typedef _list_node<T>* node_type_ptr;
+  //typedef _list_iterator<T, T&, T*> iterator;
+  typedef _list_iterator<T, const T&, const T*> const_iterator;
+  typedef _list_iterator<T, Ref, Ptr> self;
+
+  typedef T  value_type;
+  typedef Ref reference;//it will return a const refrence if const_iterator
+  typedef Ptr pointer;
+
+public:
+  _list_iterator(node_type_ptr node_ptr):node_ptr_(node_ptr)
+  {
+
+  }
+
+  self& operator++() 
+  {
+    node_ptr_ = node_ptr_->after;
+    return *this;
+  }
+
+  self operator++(int) 
+  {
+    self tmp = *this;
+    ++*this;
+    return tmp;
+  }
+
+  self& operator--() 
+  {
+    node_ptr_ = node_ptr_->before;
+    return *this;
+  }
+
+  self operator--(int) 
+  {
+    self tmp = *this;
+    --*this;
+    return tmp;
+  }
+
+  reference operator*() 
+  {
+    return (*node_ptr_).data;
+  }
+
+  pointer operator->() 
+  {
+    return &(operator*());
+  }
+
+  bool operator==(const self &itr)  //self is iterator or const_iterator
+  {
+    return node_ptr_ == itr.node_ptr_;
+  }
+
+  bool operator!=(const self &itr) 
+  {
+    return node_ptr_ != itr.node_ptr_;
+  }
+
+};
+
+
+template <typename T, typename Alloc = alloc>
+class list
+{
+
+private:
+  typedef _list_node<T> list_node;
+  typedef allocator<list_node,Alloc> _allocator;
+
+
+public:
+  typedef _list_iterator<T, T&, T*> iterator;
+  typedef _list_iterator<T, const T&, const T*> const_iterator;
+
+  typedef list_node* node_type;
+  typedef size_t size_type;
+  typedef T& refrence;
+  typedef const T& const_refrence;
+
+protected:
+  node_type head_;
+
+  node_type create()
+  {
+    return _allocator::allocate();
+  }
+
+  node_type create_and_fill(const T& value)
+  {
+    node_type node = _allocator::allocate();
+    construct(&(node->data), value);
+    return node;
+  }
+
+  void destory_node(node_type node)
+  {
+    destroy(&(node->data));
+    _allocator::deallocate(node);
+  }
+
+public:
+  list():head_(create())
+  {
+    //node_ptr = get_node();
+    head_->before = head_;
+    head_->after = head_;
+  }
+
+  iterator begin() 
+  {
+    return head_->after;
+  }
+
+  const_iterator begin() const
+  {
+    return head_->after;
+  }
+
+  iterator end()
+  {
+    return head_;
+  }
+
+  const_iterator end() const
+  {
+    return head_;
+  }
+
+  refrence front()
+  {
+    return (*begin());
+  }
+
+  const_refrence front() const
+  {
+    return (*begin());
+  }
+  refrence back()
+  {
+    return (*(end() - 1));
+  }
+
+  const_refrence back() const
+  {
+    return (*(end() - 1));
+  }
+
+
+  void push_back(const T& value)
+  {
+    node_type node = create_and_fill(value);
+    node_type tail = head_->before;
+    tail->after = node;
+    node->before = tail;
+    node->after = head_;
+    head_->before = node;
+
+  }
+
+  size_t size() const
+  {
+    return distance(begin(), end());
+  }
+
+  ~list()
+  {
+    iterator itr = begin();
+    iterator end_itr = end();
+    for (; itr != end_itr; )
+    {
+      iterator tmp = itr;
+      ++itr;
+      destory_node(tmp.node_ptr_);
+    }
+    _allocator::deallocate(head_);
+  }
+
+};
+
+}
+#endif
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/test_list.cpp
@@ -0,0 +1,94 @@
+#include "../list.hpp"
+#include <iostream>
+#include "cpptest.h"
+
+using namespace std;
+
+ 
+
+struct test_list : public Test::Suite
+{
+ 
+  void test_push_back()
+  { 
+    simple::list<int> lst;
+    TEST_ASSERT( 0 == lst.size());
+    lst.push_back(1);
+    TEST_ASSERT( 1 == lst.size());
+  }
+
+  void test_iterator()
+  {
+    simple::list<int> lst;
+    for (int i = 0; i < 100; i++)
+    {
+      lst.push_back(i);
+    }
+    simple::list<int>::iterator itr = lst.begin();
+    int value = 0;
+    for (; itr != lst.end(); itr++)
+    {
+      TEST_ASSERT(value++ == (*itr)); 
+    }
+  }
+
+  void test_iterator_set()
+  {
+    simple::list<int> lst;
+    for (int i = 0; i < 100; i++)
+    {
+      lst.push_back(i);
+    }
+    simple::list<int>::iterator itr = lst.begin();
+    TEST_ASSERT(0 == (*itr)); 
+    itr++;
+    TEST_ASSERT(1 == (*itr)); 
+    *itr = 100;
+    TEST_ASSERT(100 == (*itr)); 
+    
+    const simple::list<int> &const_lst = lst;
+    //simple::list<int>::const_iterator const_itr = lst.begin();//error
+    //*const_itr = 100;
+
+    //simple::list<int>::const_iterator const_itr = const_lst.begin();//error
+    //*const_itr = 100;
+  }
+
+
+  void test_const_iterator()
+  {
+    simple::list<int> lst;
+    for (int i = 0; i < 100; i++)
+    {
+      lst.push_back(i);
+    }
+    const simple::list<int> &const_lst = lst;
+    simple::list<int>::const_iterator itr = const_lst.begin();
+    int value = 0;
+    for (; itr != const_lst.end(); itr++)
+    {
+      TEST_ASSERT(value++ == (*itr)); 
+    }
+  }
+  
+ 
+
+  test_list()
+  {
+    TEST_ADD(test_list::test_push_back);
+    TEST_ADD(test_list::test_iterator);
+    TEST_ADD(test_list::test_const_iterator);
+    TEST_ADD(test_list::test_iterator_set);
+
+
+    
+  }
+};
+
+int main()
+{
+  test_list tl;
+  Test::TextOutput output(Test::TextOutput::Verbose);
+  (tl.run(output, false));
+  return 0;
+}
